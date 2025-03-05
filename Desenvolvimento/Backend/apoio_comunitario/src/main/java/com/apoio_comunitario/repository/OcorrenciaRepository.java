3e6bf5a5d448cea5d6d56ec56cb039ff15e68d2e
nogueira. <caiodnogueiral@gmail.com>
2025-03-05 19:48:04 -0300

    Lançamento Alpha - BD
    Teste do Sistema de B.D com Spring

---
+++ Desenvolvimento/Backend/apoio_comunitario/src/main/java/com/apoio_comunitario/repository/OcorrenciaRepository.java
@@ -0,0 +1,7 @@
+package com.apoio_comunitario.repository;
+
+import com.apoio_comunitario.model.Ocorrencia;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface OcorrenciaRepository extends JpaRepository<Ocorrencia, Integer> {
+    // depois eu adiciono alguns metodo aq
+}
