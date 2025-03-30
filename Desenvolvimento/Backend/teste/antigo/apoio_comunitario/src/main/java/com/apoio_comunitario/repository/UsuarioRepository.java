ade7954e9d48de0e4e8edca7ac8d8bdb18611969
Daniel Soares <soares.dandansss@gmail.com>
2025-03-30 13:11:34 -0300

    correção de pastas

--- Desenvolvimento/Backend/teste/antigo/apoio_comunitario/src/main/java/com/apoio_comunitario/repository/UsuarioRepository.java
+++
@@ -1,8 +0,0 @@
-package com.apoio_comunitario.repository;
-
-import com.apoio_comunitario.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
-    Usuario findByNome(String nome); // Método para buscar usuário pelo nome de usuário
-    // depois eu adiciono alguns metodo aq
-}
