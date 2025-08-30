34518ea4c9f8716c3c3d6bc348a2498c7a08b4d2
KarpaAtum <linskauadesouza@gmail.com>
2025-08-30 15:08:23 -0300

    Menu 100% resolvido

--- Desenvolvimento Front-End/src/javascript/index.js
+++ Desenvolvimento Front-End/src/javascript/index.js
@@ -124,42 +124,5 @@ function formatarData(dataString) {
     } catch(e) { return dataString; }
 }
 
-// ================================
-// MENU DO USUÁRIO
-// ================================
-// Seleciona o botão que abre o menu (a div que contém a foto e o nome)
-let expandir = document.querySelector('div#expandir')
-
-let menu = document.querySelector('div#menu')
-
-let imagem = document.querySelector('div#imagem')
-
-menu.addEventListener('click', clicar)
-
-function clicar(){
-
-    menu.classList.toggle("expandir")
-
-    imagem.classList.toggle("dep")
-
-}
-
-
-// const usuarioButton = document.querySelector('#logado > div > div > a > div.flex.gap-3');
-
-// // Seleciona o menu que vai aparecer
-// const menu = document.querySelector('#expandir .menu');
-
-// // Evento de clique no usuário
-// usuarioButton.addEventListener('click', function(e){
-//     e.preventDefault(); // previne o href "#"
-//     menu.classList.toggle('expandir'); // adiciona ou remove a classe expandir
-// });
-
-
-
-// ================================
-// INICIA FETCH AUTOMATICAMENTE
-// ================================
 fetchPostagens();
 
--- Desenvolvimento Front-End/src/javascript/menu.js
+++ Desenvolvimento Front-End/src/javascript/menu.js
@@ -37,5 +37,39 @@ document.addEventListener('DOMContentLoaded', () => {
             menuNovo.classList.add('hidden');
         }
     }
+
+    let menu = document.querySelector('div#menu')
+
+    let imagem = document.querySelector('div#imagem')
+
+    menu.addEventListener('click', clicar)
+
+    function clicar(){
+
+    menu.classList.toggle("expandir")
+
+    imagem.classList.toggle("dep")
+
+    }
+
+    const botaoSair = document.getElementById('logout')
+
+    if (botaoSair) {
+        botaoSair.addEventListener('click', (e) => {
+            e.preventDefault(); // Evita comportamento padrão
+            localStorage.removeItem('usuarioLogado'); // Remove usuário
+            // Atualiza a tela
+            if (menuNovo) {
+                menuNovo.classList.remove('flex');
+                menuNovo.classList.add('hidden');
+            }
+            if (menuAntigo) {
+                menuAntigo.classList.remove('hidden');
+                menuAntigo.classList.add('flex');
+            }
+        });
+    }
+
 });
+
 
